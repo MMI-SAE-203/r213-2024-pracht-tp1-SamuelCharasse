373ee7c0db9a0d9f949e59035435fd470cdfcb50
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-04-09 15:46:28 +0200

    Mise à jour de la fonctionnalité de menu pour afficher/cacher le menu dans la page d'accueil

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { onErrorCaptured } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
+import { ref } from 'vue'
 
+const menuIsOpen = ref(false)
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
   return true
@@ -11,19 +13,27 @@ onErrorCaptured((err, instance, info) => {
 <template>
   <header>
     <button
+      @pointerdown="menuIsOpen = !menuIsOpen"
       aria-controls="mainNav"
       aria-expanded="true"
       class="rounded-full border-2 border-red-600 bg-red-300 px-2"
     >
     menu
     </button>
-    <nav id="mainNav">
+    <Transition
+    class="transition-transform duration-1000"
+    enter-from-class="-translate-x-full"
+    enter-to-class="translate-x-0"
+    leave-active-class="-translate-x-full"
+  >
+    <nav v-show="menuIsOpen" id="mainNav">
       <ul>
         <li><a href="#">item 1</a></li>
         <li><a href="#">item 2</a></li>
         <li><a href="#">item 3</a></li>
       </ul>
     </nav>
+  </Transition>   
   </header>
   <RouterView v-slot="{ Component }">
     <Suspense>
