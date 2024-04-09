9a5ed0b1eb3019a99e6859c630bd2399460320d1
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-04-09 15:40:35 +0200

    Mise à jour de la fonctionnalité de basculement pour afficher/cacher le texte dans la page d'accueil

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const menuIsOpen = ref(false)
+//
 </script>
 
 <template>
-  <button @pointerdown="menuIsOpen=!menuIsOpen">Montrer</button>
-  <h1 v-show="menuIsOpen" class="text-2xl">Bonjour monde !</h1>
+  <h1 class="text-2xl">Bonjour monde !</h1>
 </template>
