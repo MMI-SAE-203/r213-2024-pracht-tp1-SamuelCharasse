b836c2dca2ebd556d60aef8921437f081b433c44
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-04-11 15:26:20 +0200

    Mise à jour du menu pour changer la couleur du bouton de menu en indigo dans la page d'accueil

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,17 @@ onErrorCaptured((err, instance, info) => {
       @pointerdown="menuIsOpen = !menuIsOpen"
       aria-controls="mainNav"
       aria-expanded="true"
-      class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+      class="rounded-full border-2 border-indigo-500 bg-indigo-200 px-2"
     >
     menu
     </button>
     <Transition
-    class="transition-transform duration-1000"
+    class="transition-transform duration-300"
     enter-from-class="-translate-x-full"
     enter-to-class="translate-x-0"
     leave-active-class="-translate-x-full"
   >
-    <nav v-show="menuIsOpen" id="mainNav">
+    <nav v-show="menuIsOpen" id="mainNav" class="bg-cyan-500/50 w-32 rounded-r-lg text-center hover:bg-cyan-500">
       <ul>
         <li><RouterLink to="/">Accueil</RouterLink></li>
         <li><RouterLink to="/accordeon">Accordéon</RouterLink></li>
